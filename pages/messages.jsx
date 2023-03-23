184a674431fc14f61bcaf6121c6a0166a38bb9f5
RubenStark <rubenlop_navarrete@hotmail.com>
2023-03-23 17:07:47 -0600

    MessagesPageFinished

--- pages/messages.jsx
+++ pages/messages.jsx
@@ -6,14 +6,14 @@ function Messages() {
         <>
             <div className="w-screen h-screen flex justify-center">
                 <div className="max-w-5xl w-full h-full">
-                    <div className="bg-white border border-gray-200 rounded flex h-full">
+                    <div className="border border-gray-200 rounded flex h-full">
                         {/* Left */}
-                        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 h-full max-w-sm">
+                        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 h-full max-w-sm bg-white">
 
                             {/* SearchBar */}
 
-                            <div className="row justify-center my-5">
-                                <Input clearable bordered placeholder="Name" size="lg" />
+                            <div className="row justify-center mx-2 my-5">
+                                <Input clearable placeholder="Name" size="lg"/>
                             </div>
                             {/* Chats */}
                             <ul className="py-1 overflow-auto">
@@ -95,9 +95,9 @@ function FullMessages() {
 
     var [messages, setMessages] = useState(null)
 
-    if (messages) {
+    if (!messages) {
         return (
-            <div className="w-full bg-gray-100 overflow-scroll">
+            <div className="w-full bg-white overflow-scroll">
                 {/* Messages */}
                 <div className="w-full flex flex-col-reverse">
                     <div className="m-5"></div>
@@ -110,8 +110,29 @@ function FullMessages() {
                     <Message />
                     <Message />
 
-                    <div className="w-full bottom-0 fixed">
-                        <textarea className="w-full" placeholder="Type a message" />
+                    <div className="w-full bottom-0 fixed max-w-3xl">
+                        <Input
+                            placeholder="Enter Your Message"
+                            fullWidth="true"
+                            status="primary"
+                            clearable
+                            contentRight={
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                                    />
+                                </svg>
+                            }
+                        />;
                     </div>
                 </div>
             </div>
@@ -136,26 +157,4 @@ function FullMessages() {
         </div>
 
     )
-}
-
-function SearchBar() {
-
-    return (
-        <form className="w-full">
-            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                </div>
-                <input
-                    type="search"
-                    id="default-search"
-                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search for a Chat"
-                    required />
-                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-            </div>
-        </form>
-    )
-
 }
