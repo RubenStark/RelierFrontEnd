e389b8635de1008e7831a3e2d406592f4b15a152
RubenStark <rubendehca321@outlook.com>
2023-06-05 17:23:46 -0600

    following context

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-import { Navbar, Button, Link, } from "@nextui-org/react";
+import React, { useContext, useEffect } from "react";
+import { Navbar, Button, Link } from "@nextui-org/react";
+import { FollowingForYouContext } from "../utils/context";
+import { useAuth } from "../hooks/useAuth";
 
+function MyNavbar({ handleFollowing }) {
+  const { setFollowing } = useContext(FollowingForYouContext);
 
-function MyNavbar() {
-    return (
+  const handleToggleFollowing = () => {
+    setFollowing(true);
+  };
+  const handleToggleForYou = () => {
+    setFollowing(false);
+  };
 
-        <Navbar variant="sticky" maxWidth={'fluid'}>
-            <Navbar.Brand>
-                <Link href="/">
-                    <span className='font-bold text-xl'>Relier</span>
-                </Link>
-            </Navbar.Brand>
-            <Navbar.Content hideIn="xs" variant="underline">
-                <Navbar.Link href="#">Following</Navbar.Link>
-                <Navbar.Link isActive href="#">For You</Navbar.Link>
-            </Navbar.Content>
-            <Navbar.Content>
-                <Navbar.Link color="inherit" href="/login">
-                    <Button auto flat as={Link} href="/login">
-                        Login | Sign Up
-                    </Button>
-                </Navbar.Link>
-            </Navbar.Content>
-        </Navbar>
+  const user = useAuth();
 
-    )
+  return (
+    <Navbar variant="sticky" maxWidth={"fluid"}>
+      <Navbar.Brand>
+        <Link href="/">
+          <span className="font-bold text-xl">Relier</span>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Content hideIn="xs" variant="underline">
+        <Navbar.Link href="#" onPress={user && handleToggleFollowing}>
+          Following
+        </Navbar.Link>
+        <Navbar.Link isActive href="#" onPress={user && handleToggleForYou}>
+          For You
+        </Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link color="inherit" href="/login">
+          <Button auto flat as={Link} href="/login">
+            Login | Sign Up
+          </Button>
+        </Navbar.Link>
+      </Navbar.Content>
+    </Navbar>
+  );
 }
 
-export default MyNavbar
+export default MyNavbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,22 @@
-import Lateral from '../components/Lateral/lateral'
-import MyNavbar from '../components/navbar'
-import BottomBar from '../components/bottomBar'
+import Lateral from "../components/Lateral/lateral";
+import MyNavbar from "../components/navbar";
+import BottomBar from "../components/bottomBar";
 
-import '../styles/globals.css'
-import '../styles/login.css'
+import "../styles/globals.css";
+import "../styles/login.css";
 import "react-toastify/dist/ReactToastify.css";
 
-import { useRouter } from 'next/router';
-import { NextUIProvider } from '@nextui-org/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { use, useEffect, useState } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { AuthContext } from '../utils/context'
-import { isUserLogedApi } from '../api/auth'
+import { useRouter } from "next/router";
+import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { use, useEffect, useMemo, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { AuthContext, FollowingForYouContext } from "../utils/context";
+import { isUserLogedApi } from "../api/auth";
 
 export default function App({ Component, pageProps }) {
-
   const router = useRouter();
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   const [user, setUser] = useState(null);
   const [loadUser, setLoadUser] = useState(false);
@@ -29,9 +28,7 @@ export default function App({ Component, pageProps }) {
     setLoadUser(true);
   }, [refreshCheckLogin]);
 
-
   if (!loadUser) return null;
-
 
   if (router.pathname === "/login" || router.pathname === "/register") {
     return (
@@ -52,7 +49,7 @@ export default function App({ Component, pageProps }) {
           </AuthContext.Provider>
         </NextUIProvider>
       </QueryClientProvider>
-    )
+    );
   }
 
   return (
@@ -60,24 +57,52 @@ export default function App({ Component, pageProps }) {
       <NextUIProvider>
         {/* App */}
         <AuthContext.Provider value={{ user, setUser }}>
-          <MyNavbar />
-          <div className="my-5"></div>
-          <Lateral />
-          <Component {...pageProps} />
-          <BottomBar />
-          <ToastContainer
-            position="top-right"
-            autoClose={2500}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            draggable
-            pauseOnHover={false}
-          />
+          <FollowingProvider>
+            <MyNavbar />
+            <div className="my-5"></div>
+            <Lateral />
+            <Component {...pageProps} />
+            <BottomBar />
+            <ToastContainer
+              position="top-right"
+              autoClose={2500}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              draggable
+              pauseOnHover={false}
+            />
+          </FollowingProvider>
         </AuthContext.Provider>
         {/* App */}
       </NextUIProvider>
     </QueryClientProvider>
-  )
+  );
+}
+
+// TODO : Implementar el contexto de following
+// const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+
+//   const contextValue = useMemo(() => ({ user, setUser }), [user]);
+
+//   return (
+//     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+//   );
+// };
+
+const FollowingProvider = ({ children }) => {
+  const [following, setFollowing] = useState(null);
+
+  const contextValue = useMemo(
+    () => ({ following, setFollowing }),
+    [following]
+  );
+
+  return (
+    <FollowingContext.Provider value={contextValue}>
+      {children}
+    </FollowingContext.Provider>
+  );
 };
