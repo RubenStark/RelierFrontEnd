dc3fc8673defd6aae31473b38db07ec62de0ba3b
RubenStark <rubendehca321@outlook.com>
2023-06-21 18:23:50 -0600

    Buttons, Stories, Scroll bar

--- components/Stories/story.jsx
+++ components/Stories/story.jsx
@@ -1,11 +1,21 @@
+import { Avatar } from "@nextui-org/react";
+
 function Story() {
-    return (
-        <img
-            className="hover:opacity-90 cursor-pointer transition duration-200 ease-in-out mr-5 rounded-xl"
-            src="https://picsum.photos/id/29/200/300"
-            style={{ width: '136px' }}
+  return (
+    <div className="relative" style={{ width: "136px" }}>
+      <img
+        className="hover:opacity-90 cursor-pointer transition duration-200 ease-in-out rounded-xl"
+        src="https://picsum.photos/id/29/200/300"
+        style={{ width: "136px" }}
+      />
+      <div className="absolute bottom-2 left-2">
+        <Avatar
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            size="md"
         />
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Story
+export default Story;
